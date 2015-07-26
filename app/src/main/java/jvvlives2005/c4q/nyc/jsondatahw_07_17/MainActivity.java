6fc3db436c427debadcdd72dcc1df38801286479
jvvlives2005 <jvvlives2005@aol.com>
2015-07-26 12:13:22 -0400

    notification added for homework 07-24

--- app/src/main/java/jvvlives2005/c4q/nyc/jsondatahw_07_17/MainActivity.java
+++ app/src/main/java/jvvlives2005/c4q/nyc/jsondatahw_07_17/MainActivity.java
@@ -1,17 +1,66 @@
 package jvvlives2005.c4q.nyc.jsondatahw_07_17;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final int NOTIFICATION_ID = 1234;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showHSDataActivity();
+        showNotification();
+    }
+
+    private void showNotification()
+    {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss a");
+        updateNotification("Time is...", simpleDateFormat.format(new Date()));
+    }
+
+    private void updateNotification(String titleText, String contentText)
+    {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setAutoCancel(true);
+        builder.setContentTitle(titleText);
+        builder.setContentText(contentText);
+        Intent resultIntent = new Intent(this, HSDataActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void cancelNotification()
+    {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void showHSDataActivity() {
+        Intent intent = new Intent(this, HSDataActivity.class);
+        ListView listView = (ListView) findViewById(R.id.list_item);
+        startActivity(intent);
+
     }
 
 
